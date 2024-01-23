dc3450273d1866e560c44a9c2222cf3dd260d419
Eduardo O D <edudobbinshotmail.com@gmail.com>
2024-01-23 16:39:17 -0400

    Correção no Js

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ $(document).ready(function() {
 
     $('form').on('submit', function(e){
         e.preventDefault()
-        const enderecoDaNovaImagem = $('endereco-de-imagem-nova').val()
+        const enderecoDaNovaImagem = $('#endereco-de-imagem-nova').val()
         const novoItem = $('<li style="display: none"></li>')
-        $(`<img src="${enderecoDaNovaImagem}">`).apendTo(novoItem)
+        $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem)
         $(`<div class="overlay-imagem-link">
             <a href="${enderecoDaNovaImagem}" title="Ver imagem em tamanho real" target="_blanck">Ver imagem em tamanho real</a>
-        </div>`).apendTo(novoItem)
-        $(novoItem).apendTo('ul')
+        </div>`).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
         $(novoItem).fadeIn(1000)
         $('#endereco-de-imagem-nova').val('')
     })
