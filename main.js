a8f98f08e1732ff660cfb7711614c25f07dedf29
Eduardo O D <edudobbinshotmail.com@gmail.com>
2024-01-23 16:18:06 -0400

    furst commit

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('header button').click(function(){
+        $('form').slideDown()
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp()
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault()
+        const enderecoDaNovaImagem = $('endereco-de-imagem-nova').val()
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<img src="${enderecoDaNovaImagem}">`).apendTo(novoItem)
+        $(`<div class="overlay-imagem-link">
+            <a href="${enderecoDaNovaImagem}" title="Ver imagem em tamanho real" target="_blanck">Ver imagem em tamanho real</a>
+        </div>`).apendTo(novoItem)
+        $(novoItem).apendTo('ul')
+        $(novoItem).fadeIn(1000)
+        $('#endereco-de-imagem-nova').val('')
+    })
+})
